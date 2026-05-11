0cfdd10297eaa9bd5d3df472f11b0b061c95e778
Nick L <iamn1ck@yahoo.com>
2026-05-11 12:17:50 +0200

    add shared library to appimage

--- ci/build-appimage.sh
+++ ci/build-appimage.sh
@@ -21,6 +21,13 @@ done
 cp -r platforms/freedesktop/{16x16,32x32,48x48,64x64,128x128,256x256,512x512,1024x1024} build/appdir/usr/share/icons/hicolor
 cp platforms/freedesktop/dusk.desktop build/appdir/usr/share/applications
 
+OPENXR_LIB=$(find "$GITHUB_WORKSPACE"/build -name "libopenxr_loader.so.1" | head -n 1)
+
 cd build/install
+<<<<<<< HEAD
 VERSION="$DUSK_VERSION" NO_STRIP=1 "$linuxdeploy" \
   -l /usr/lib/x86_64-linux-gnu/libusb-1.0.so --appdir "$build_dir/appdir" --output appimage
+=======
+VERSION="$DUSK_VERSION" NO_STRIP=1 "$RUNNER_WORKSPACE"/linuxdeploy-$(uname -m).AppImage \
+  -l /usr/lib/x86_64-linux-gnu/libusb-1.0.so -l "$OPENXR_LIB" --appdir "$GITHUB_WORKSPACE"/build/appdir --output appimage
+>>>>>>> 825b4c5b6d (add shared library to appimage)
